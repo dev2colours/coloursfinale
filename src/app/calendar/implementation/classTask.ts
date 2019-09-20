85e42311c9cdbab20b77ce072ca07e4a1fdcc5f7
dev2colours <dev2colours@gmail.com>
2019-09-20 17:09:26 +0200

    reset
    reset

--- src/app/calendar/implementation/classTask.ts
+++
@@ -1,5 +0,0 @@
-import { Task } from '../../models/task-model';
-import { classification } from 'app/models/user-model';
-export interface classTask extends Task {
-  classification: classification;
-}
